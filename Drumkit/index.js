ee7b6a6b7f9fb179b8bb2d3a99490c226c722769
Aaditya <aadityapuriaaditya@gmail.com>
2023-10-24 16:45:23 +0530

    Drumkit Project

---
+++ Drumkit/index.js
@@ -0,0 +1,60 @@
+// document.querySelector("button").addEventListener("click", 
+// function (){
+//   alert("I got clicked");
+// });
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+    makeSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+  });
+}
+
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    default:
+      console.log(this.innerHTML);
+      break;
+  }
+}
+
+function buttonAnimation(key){
+  document.querySelector("."+key).classList.add("pressed");
+  document.querySelector("."+key).style.color="#dbedf3";
+  setTimeout(function(){
+    document.querySelector("."+key).classList.remove("pressed");
+    document.querySelector("."+key).style.color="#DA0463";
+  }, 100)
+}
