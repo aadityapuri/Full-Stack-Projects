00624d1dfc69c2085a007b882d6cc1d8398a26a6
Aaditya <aadityapuriaaditya@gmail.com>
2023-10-26 19:31:52 +0530

    Intro to Node

---
+++ intro-node/index.js
@@ -0,0 +1,8 @@
+// jshint esversion:6
+var superheroes = require("superheroes");
+var mysuperhero = superheroes.random();
+console.log(mysuperhero);
+
+var supervillain = require("supervillains");
+var mysupvillain = supervillain.random();
+console.log(mysupvillain);
